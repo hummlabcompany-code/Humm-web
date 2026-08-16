133d0e3e2fc9b04774c7e576946fb5b16bbc5283
Humm Lab <hummlabcompany-code@users.noreply.github.com>
2026-08-16 17:46:22 +0700

    fix: apply brand typography across all routes

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -4,4 +4,5 @@ import "./commerce.css";
 import "./trust.css";
 import "./shopify.css";
 import "./enhancements.css";
+import "../fonts.css";
 export default function ShopLayout({children}:{children:React.ReactNode}){return <CartProvider>{children}</CartProvider>}
